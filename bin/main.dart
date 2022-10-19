3ffc2a954bf1e5730746230e5c516eb55ee0967e
ramez1212 <317170274@sha.edu.eg>
2022-10-19 12:57:19 +0200

    applying the Async and Await

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,18 @@
-void main(List<String> args) {
+void main(List<String> args) async {
 //* Future operation without async and await to prevent make the most innermost function async
-  final futureSum = Future.delayed(Duration(seconds: 1), sum)
-      .then((value) => print(value))
-      .catchError((onError) => print(onError));
+//   final futureSum = Future.delayed(Duration(seconds: 1), sum)
+//       .then((value) => print(value))
+//       .catchError((onError) => print(onError));
+
+//! implementing the above sum using async and await (notice any Future<dynamic> will be dynamic using await)
+//* the try and catch here behaves as same as .catchError() in Future
+  try {
+    final asyncFutureSum = await Future.delayed(Duration(milliseconds: 2), sum);
+    //* the below statement behaves as same as .then() in future
+    print(asyncFutureSum);
+  } catch (error) {
+    print("returned asyncFutureSum operation with error: $error");
+  }
 }
 
 double sum() => 2.5 + 3.0;
